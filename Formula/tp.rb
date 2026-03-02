818ed1cabe332c9aa33e004cbf62ccf0695701da
Patrick Tumbucon <patricktumbucon@gmail.com>
2026-03-02 09:49:07 +0000

    feat: add sha256 hashes for v0.1.0 release

--- Formula/tp.rb
+++ Formula/tp.rb
@@ -7,17 +7,20 @@ class Tp < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/pattynextdoor/tp/releases/download/v#{version}/tp-macos-aarch64.tar.gz"
-      # sha256 will be filled after first release
+      sha256 "77e709269f4c1b30da2f4051dea070ecde895768c143bd9b75c9867167991899"
     else
       url "https://github.com/pattynextdoor/tp/releases/download/v#{version}/tp-macos-x86_64.tar.gz"
+      sha256 "38b76f21d82e593da888952fad0448d377b7c7da12570a041a6615a6cf66b597"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/pattynextdoor/tp/releases/download/v#{version}/tp-linux-aarch64.tar.gz"
+      sha256 "dca412145e7b764caf603ffb01c3de9ddfb9c5b427f30cc8e845130a2f5fe33c"
     else
       url "https://github.com/pattynextdoor/tp/releases/download/v#{version}/tp-linux-x86_64.tar.gz"
+      sha256 "46fee2723354319682f2692efa7c25d6d5fbc114ed346d611d359da6d9834ce0"
     end
   end
 
